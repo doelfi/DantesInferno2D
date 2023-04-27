15eb2562fdb59148c8c88484f3280a9c67364c2b
hwarnecke <82388774+hwarnecke@users.noreply.github.com>
2023-04-27 13:16:33 +0200

    feature(Player) onLevelSwitch is now called by the finish line to reset the levelStats

--- Assets/scripts/Finishline.cs
+++ Assets/scripts/Finishline.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Finishline : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject player;
+    private PlayerController playerScript;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerScript = player.GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
@@ -21,6 +25,7 @@ public class Finishline : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             UnityEngine.Debug.Log("finish line reached");
+            playerScript.onLevelSwitch();
         }
     }
 }
