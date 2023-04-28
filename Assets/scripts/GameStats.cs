61a332dba0c43887836d283c83b52785e2178e30
hwarnecke <82388774+hwarnecke@users.noreply.github.com>
2023-04-28 12:01:37 +0200

    Bugfix(Stats) removed fixme

--- Assets/scripts/GameStats.cs
+++ Assets/scripts/GameStats.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public static class GameStats
 {
-    // FIXME: need a different way of setting the stats at the start of the game. 
-    // This way on level load everything gets resetted to (3,0).
     public static int lifes = 3;
     public static int coins = 0;
 }
