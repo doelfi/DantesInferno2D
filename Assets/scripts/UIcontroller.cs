4ff65fb2405acd0ebfbcd395a47f03025dfb5439
hwarnecke <82388774+hwarnecke@users.noreply.github.com>
2023-04-28 12:03:00 +0200

    Bugfix(UI) needs to be updated on start of the game

--- Assets/scripts/UIcontroller.cs
+++ Assets/scripts/UIcontroller.cs
@@ -9,6 +9,13 @@ public class UIcontroller : MonoBehaviour
 
     public TMP_Text panel;
 
+    // FIXME: I don't know why but this does not get called on start up
+    // My current fix is to call the updateUI() function from the start of the player
+    void start()
+    {
+        panel.text = "Lifes: " + GameStats.lifes.ToString() + " Coins: " + GameStats.coins.ToString();
+    }
+
     public void updateUI()
     {
         panel.text = "Lifes: " + GameStats.lifes.ToString() + " Coins: " + GameStats.coins.ToString();
