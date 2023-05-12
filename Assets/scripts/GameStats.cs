96cd21b08b4940e645a77f4f41c32b22a1f0a720
hwarnecke <82388774+hwarnecke@users.noreply.github.com>
2023-05-12 11:14:12 +0200

    feature(Stats) added function to save run Times

--- Assets/scripts/GameStats.cs
+++ Assets/scripts/GameStats.cs
@@ -1,14 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public static class GameStats
 {
     // player stats
     public static int lifes = 3;
     public static int coins = 0;
-
-
+    
     // speed run times
     public static double[] runTimes = new double[6];
+    public static string runTimePath = "";
+
+    // write the runTimes to the file to save them
+    // should be called on level switch from the player
+    public static void SaveRunTimes()
+    {
+        File.WriteAllText(runTimePath, ""); // delete old content of the file
+
+        foreach (double entry in runTimes)
+        {
+            File.AppendAllText(runTimePath, entry.ToString());
+        }
+    }
 }
