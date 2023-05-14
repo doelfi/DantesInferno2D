5ab5326791ed472bb2e8d1620fc657bc6b24fac7
hwarnecke <82388774+hwarnecke@users.noreply.github.com>
2023-05-14 17:17:16 +0200

    feature(falling Spike) added Tag for bottom line

--- Assets/scripts/RemoveFallingSpikes.cs
+++ Assets/scripts/RemoveFallingSpikes.cs
@@ -6,7 +6,7 @@ public class RemoveFallingSpikes : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Enemy"))
+        if (other.CompareTag("Enemy") || other.CompareTag("BottomLine"))
         {
             Destroy(this.gameObject);
         }
