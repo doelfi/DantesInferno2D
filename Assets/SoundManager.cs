1866211c484082a687a9abadaf9707a1b5a1b00c
hwarnecke <82388774+hwarnecke@users.noreply.github.com>
2023-05-22 22:00:35 +0200

    feature (Sound) added script to manage sound
    has to still be filled with assets and made into prefab

---
+++ Assets/SoundManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    private AudioSource audioSource;
+    
+    // all possible Sound Effects
+    public enum SoundOptions
+    {
+        PlayerJump,
+        PlayerMove,
+        PlayerLand,
+        CoinCollected,
+        LifeGained,
+        DamageTaken,
+        EnemyKilled,
+        FinishReached,
+    }
+    
+    // this simulates a dictionary in the editor
+    // since dictionaries can not be filled over the editor
+    [System.Serializable]
+    public struct AudioClips
+    {
+        public SoundManager.SoundOptions name;
+        public AudioClip clip;
+    }
+    
+    public AudioClips[] SoundAssets;
+    
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    
+    // takes one of the Options listed above and plays it once
+    public void PlaySound(SoundOptions name)
+    {
+        foreach (AudioClips entry in SoundAssets)
+        {
+            if (entry.name == name)
+            {
+                audioSource.clip = entry.clip;
+                audioSource.Play();
+                return;
+            }
+        }
+        UnityEngine.Debug.LogError("Clip " + name + " not found");
+    }
+}
