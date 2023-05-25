fdeef0985b1d010f8f30ccfbebbc6bad1c861fce
hwarnecke <82388774+hwarnecke@users.noreply.github.com>
2023-05-25 11:55:40 +0200

    feature(Platform) added disappearing animation for platform

---
+++ Assets/scripts/Platform/TrapPlatform.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapPlatform : MonoBehaviour
+{
+    private bool _triggered = false;
+    private SpriteRenderer _mSpriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _mSpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!_triggered)
+        {
+           StartCoroutine(DisappearAnimation()); 
+        }
+        _triggered = true;
+    }
+
+    IEnumerator DisappearAnimation()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            float alpha = 1 - (i / 10f);
+            _mSpriteRenderer.material.SetColor("_Color", new Color(1f,1f,1f,alpha));
+            foreach (Transform child in transform)
+            {
+                child.gameObject.GetComponent<SpriteRenderer>().material.SetColor("_Color", new Color(1f,1f,1f,alpha));
+            }
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
